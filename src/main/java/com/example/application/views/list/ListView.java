3dfbc39bdecedd51b7da92b93215e35f156a2a9a
WoodyGray <115577474+WoodyGray@users.noreply.github.com>
2023-09-13 21:42:57 +0300

    commite

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -29,9 +29,10 @@ public class ListView extends VerticalLayout {
 
         add(getToolbar(), getContent());
         updateList();
+        closeEditor();
     }
 
-    private Component getContent(){
+    private HorizontalLayout getContent(){
         HorizontalLayout content = new HorizontalLayout(grid, form);
         content.setFlexGrow(2, grid);
         content.setFlexGrow(1, form);
@@ -47,12 +48,31 @@ public class ListView extends VerticalLayout {
         grid.addColumn(contact -> contact.getStatus().getName()).setHeader("Status");
         grid.addColumn(contact -> contact.getCompany().getName()).setHeader("Company");
         grid.getColumns().forEach(col -> col.setAutoWidth(true));
+
+        grid.asSingleSelect().addValueChangeListener(event ->
+                editContact(event.getValue()));
     }
 
     private void configureForm(){
         form = new ContactForm(service.findAllCompanies(), service.findAllStatuses());
         form.setWidth("25em");
+        form.addSaveListener(this::saveContact);
+        form.addDeleteListener(this::deleteContact);
+        form.addCloseListener(e -> closeEditor());
     }
+
+    private void saveContact(ContactForm.SaveEvent event){
+        service.saveContact(event.getContact());
+        updateList();
+        closeEditor();
+    }
+
+    private void deleteContact(ContactForm.DeleteEvent event){
+        service.deleteContact(event.getContact());
+        updateList();
+        closeEditor();
+    }
+
 
     private HorizontalLayout getToolbar(){
         filterText.setPlaceholder("Filter by name...");
@@ -61,10 +81,32 @@ public class ListView extends VerticalLayout {
         filterText.addValueChangeListener(e -> updateList());
 
         Button addContactButton = new Button("Add contact");
+        addContactButton.addClickListener(click -> addContact());
 
         var toolbar = new HorizontalLayout(filterText, addContactButton);
         toolbar.addClassNames("toolbar");
         return toolbar;
+    }
+
+    public void editContact(Contact contact){
+        if (contact == null){
+            closeEditor();
+        }else {
+            form.setContact(contact);
+            form.setVisible(true);
+            addClassName("editing");
+        }
+    }
+
+    private void closeEditor() {
+        form.setContact(null);
+        form.setVisible(false);
+        removeClassName("editing");
+    }
+
+    private void addContact(){
+        grid.asSingleSelect().clear();
+        editContact(new Contact());
     }
 
     private void updateList(){
